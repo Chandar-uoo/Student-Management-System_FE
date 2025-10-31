0fc76cf2f043ca6f7ea6e3125cbb9c3569ecaafd
Chandar-uoo <chandru666@vgmail.com>
2025-10-31 18:12:42 +0530

    feat: refactor App component to include routing for login, signup, dashboard, and student pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+import { DashBoard } from "./pages/DashBoard";
+import { StudentFormWrapper } from "./pages/StudentFormWrapper";
+import { StudentDetail } from "./pages/StudentDetail";
 
 function App() {
-
-
   return (
     <>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<DashBoard />} />
+          <Route path="/student-form" element={<StudentFormWrapper />} />
+          <Route path="/student" element={<StudentDetail />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
